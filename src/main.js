0308ddeb3fd8965eee142bf28d438be3026560aa
chenxuexin <2338282460@qq.com>
2026-03-10 00:27:15 +0800

    feat：组件使用中文包

--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 // 导入 Element Plus
-import ElementPlus from 'element-plus'
+import ElementPlus from "element-plus";
 // 导入自定义主题色配置
-import './assets/style/element-variables.scss'
-import 'element-plus/dist/index.css'
+import "./assets/style/element-variables.scss";
+import "element-plus/dist/index.css";
 // 导入 Element Plus 图标
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 // 导入初始化样式
-import './assets/style/global.css'
+import "./assets/style/global.css";
 // 中文
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-const app = createApp(App)
-const pinia = createPinia()
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
-app.use(router)
-app.use(ElementPlus)
+app.use(pinia);
+app.use(router);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 // 注册所有图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+  app.component(key, component);
 }
-app.mount('#app')
+app.mount("#app");
